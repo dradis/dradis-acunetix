ec021a23d4395edaa90f0f8f2c758c290f0bd985
Daniel Martin <etd-github@nomejortu.com>
2014-11-07 12:14:54 +0100

    Rudimentary FieldProcessor

--- lib/dradis/plugins/acunetix/field_processor.rb
+++ lib/dradis/plugins/acunetix/field_processor.rb
@@ -1,31 +1,29 @@
 module Dradis::Plugins::Acunetix
   class FieldProcessor < Dradis::Plugins::Upload::FieldProcessor
 
-    # def post_initialize(args={})
-    #   @nessus_object = (data.name == 'ReportHost') ? ::Nessus::Host.new(data) : ::Nessus::ReportItem.new(data)
-    # end
-    #
-    # def value(args={})
-    #   field = args[:field]
-    #
-    #   # fields in the template are of the form <foo>.<field>, where <foo>
-    #   # is common across all fields for a given template (and meaningless).
-    #   _, name = field.split('.')
-    #
-    #   if name.end_with?('entries')
-    #     # report_item.bid_entries
-    #     # report_item.cve_entries
-    #     # report_item.xref_entries
-    #     entries = @nessus_object.try(name)
-    #     if entries.any?
-    #       entries.to_a.join("\n")
-    #     else
-    #       'n/a'
-    #     end
-    #   else
-    #     @nessus_object.try(name) || 'n/a'
-    #   end
-    # end
+    def post_initialize(args={})
+      # @acunetix_object = (data.name == 'Scan') ? ::Nessus::Host.new(data) : ::Nessus::ReportItem.new(data)
+    end
+
+    def value(args={})
+      field = args[:field]
+
+      # fields in the template are of the form <foo>.<field>, where <foo>
+      # is common across all fields for a given template (and meaningless).
+      _, name = field.split('.')
+
+      tag_name = if name == 'start_url'
+                  'StartURL'
+                else
+                  name.camelcase
+                end
+
+      if tag = data.at_xpath("./#{tag_name}")
+        tag.text()
+      else
+        'n/a'
+      end
+    end
   end
 
 end
