41ee6e30ee3f1ba7e62cb93c5a4b18f0246fac15
Daniel Martin <etd-github@nomejortu.com>
2014-11-07 14:19:40 +0100

    Add a temporary Thorfile

---
+++ lib/tasks/thorfile.rb
@@ -0,0 +1,48 @@
+module Dradis
+  module Plugins
+    module Acunetix
+
+      class Thor < Thor
+include Core::Pro::ProjectScopedTask
+        namespace     "dradis:plugins:acunetix"
+
+        desc      "upload FILE", "upload Acunetix XML results"
+        def upload(file_path)
+          require 'config/environment'
+
+          logger = Logger.new(STDOUT)
+          logger.level = Logger::DEBUG
+
+          unless File.exists?(file_path)
+            $stderr.puts "** the file [#{file_path}] does not exist"
+            exit -1
+          end
+
+detect_and_set_project_scope
+
+          content_service = nil
+          template_service = nil
+          if defined?(Dradis::Pro)
+            content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::Acunetix)
+            template_service = Dradis::Pro::Plugins::TemplateService.new(plugin: Dradis::Plugins::Acunetix)
+          else
+            content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::Acunetix)
+            template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::Acunetix)
+          end
+
+          importer = Dradis::Plugins::Acunetix::Importer.new(
+                      logger: logger,
+             content_service: content_service,
+            template_service: template_service
+          )
+
+          importer.import(file: file_path)
+
+          logger.close
+        end
+
+      end
+
+    end
+  end
+end
