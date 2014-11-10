b47f05c205722f14d8060519016ddda2987617c5
Daniel Martin <etd-github@nomejortu.com>
2014-11-10 12:56:28 +0100

    Change module name (copy&paste error) s/Nessus/Acunetix/

--- lib/dradis/plugins/acunetix/version.rb
+++ lib/dradis/plugins/acunetix/version.rb
@@ -2,8 +2,8 @@ require_relative 'gem_version'
 
 module Dradis
   module Plugins
-    module Nessus
-      # Returns the version of the currently loaded Nessus as a
+    module Acunetix
+      # Returns the version of the currently loaded Acunetix as a
       # <tt>Gem::Version</tt>.
       def self.version
         gem_version
