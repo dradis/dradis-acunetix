f2bef4e50f21352c3e0a8afd50ce2fca71169b36
Daniel Martin <etd-github@nomejortu.com>
2014-11-21 16:21:01 +0100

    Fix module tree - can't use VERSION before requiring it below

--- lib/dradis/plugins/acunetix.rb
+++ lib/dradis/plugins/acunetix.rb
@@ -1,8 +1,21 @@
 module Dradis
   module Plugins
     module Acunetix
-      # This is required while we transition the Upload Manager to use
-      # Dradis::Plugins only
+    end
+  end
+end
+
+require 'dradis/plugins/acunetix/engine'
+require 'dradis/plugins/acunetix/field_processor'
+require 'dradis/plugins/acunetix/importer'
+require 'dradis/plugins/acunetix/version'
+
+
+# This is required while we transition the Upload Manager to use
+# Dradis::Plugins only
+module Dradis
+  module Plugins
+    module Acunetix
       module Meta
         NAME = "Acunetix XML upload plugin"
         EXPECTS = "Acunetix XML format."
@@ -13,8 +26,3 @@ module Dradis
     end
   end
 end
-
-require 'dradis/plugins/acunetix/engine'
-require 'dradis/plugins/acunetix/field_processor'
-require 'dradis/plugins/acunetix/importer'
-require 'dradis/plugins/acunetix/version'
