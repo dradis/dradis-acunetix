3818dd1b4a691fa531832060bf410d00b1d0b756
Daniel Martin <etd-github@nomejortu.com>
2014-11-25 13:56:20 +0100

    Thorfile - fix tasks so they can be loaded from the main app

--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -1,48 +1,40 @@
-module Dradis
-  module Plugins
-    module Acunetix
+class AcunetixTasks < Thor
+  include Core::Pro::ProjectScopedTask if defined?(::Core::Pro)
+  
+  namespace "dradis:plugins:acunetix"
 
-      class Thor < Thor
-include Core::Pro::ProjectScopedTask
-        namespace     "dradis:plugins:acunetix"
+  desc "upload FILE", "upload Acunetix XML results"
+  def upload(file_path)
+    require 'config/environment'
 
-        desc      "upload FILE", "upload Acunetix XML results"
-        def upload(file_path)
-          require 'config/environment'
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::DEBUG
 
-          logger = Logger.new(STDOUT)
-          logger.level = Logger::DEBUG
-
-          unless File.exists?(file_path)
-            $stderr.puts "** the file [#{file_path}] does not exist"
-            exit -1
-          end
-
-detect_and_set_project_scope
-
-          content_service = nil
-          template_service = nil
-          if defined?(Dradis::Pro)
-            content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::Acunetix)
-            template_service = Dradis::Pro::Plugins::TemplateService.new(plugin: Dradis::Plugins::Acunetix)
-          else
-            content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::Acunetix)
-            template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::Acunetix)
-          end
-
-          importer = Dradis::Plugins::Acunetix::Importer.new(
-                      logger: logger,
-             content_service: content_service,
-            template_service: template_service
-          )
-
-          importer.import(file: file_path)
-
-          logger.close
-        end
-
-      end
-
+    unless File.exists?(file_path)
+      $stderr.puts "** the file [#{file_path}] does not exist"
+      exit -1
     end
+
+    content_service = nil
+    template_service = nil
+    if defined?(Dradis::Pro)
+      detect_and_set_project_scope
+      content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::Acunetix)
+      template_service = Dradis::Pro::Plugins::TemplateService.new(plugin: Dradis::Plugins::Acunetix)
+    else
+      content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::Acunetix)
+      template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::Acunetix)
+    end
+
+    importer = Dradis::Plugins::Acunetix::Importer.new(
+                logger: logger,
+       content_service: content_service,
+      template_service: template_service
+    )
+
+    importer.import(file: file_path)
+
+    logger.close
   end
+
 end
