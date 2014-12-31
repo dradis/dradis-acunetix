2b820f19b02e817ae59d95fac246baaf7cd79293
Daniel Martin <etd-github@nomejortu.com>
2014-12-31 11:30:46 +0100

    First stab at specs

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'nokogiri'
+
+require 'combustion'
+
+Combustion.initialize!
+
+RSpec.configure do |config|
+  # Enable colors
+  config.color = true
+  # Use the specified formatter
+  config.formatter = :documentation
+end
