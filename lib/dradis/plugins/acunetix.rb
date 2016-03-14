cd47128a290574fdacf4afd2444b4d803781bdb1
Daniel Martin <etd-github@nomejortu.com>
2016-03-14 12:44:32 +0100

    We no longer need a Meta module

--- lib/dradis/plugins/acunetix.rb
+++ lib/dradis/plugins/acunetix.rb
@@ -10,19 +10,3 @@ require 'dradis/plugins/acunetix/field_processor'
 require 'dradis/plugins/acunetix/importer'
 require 'dradis/plugins/acunetix/version'
 
-
-# This is required while we transition the Upload Manager to use
-# Dradis::Plugins only
-module Dradis
-  module Plugins
-    module Acunetix
-      module Meta
-        NAME = "Acunetix XML upload plugin"
-        EXPECTS = "Acunetix XML format."
-        module VERSION
-          include Dradis::Plugins::Acunetix::VERSION
-        end
-      end
-    end
-  end
-end
