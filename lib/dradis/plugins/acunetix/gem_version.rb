a99e133972047ff9116b42b00ac5ce9db7a9e04c
Rachael Korinek <rachael.korinek@gmail.com>
2017-01-26 14:29:13 -0600

    Bump tiny version number for merging

--- lib/dradis/plugins/acunetix/gem_version.rb
+++ lib/dradis/plugins/acunetix/gem_version.rb
@@ -8,8 +8,8 @@ module Dradis
 
       module VERSION
         MAJOR = 3
-        MINOR = 1
-        TINY = 1
+        MINOR = 2
+        TINY = 2
         PRE = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
