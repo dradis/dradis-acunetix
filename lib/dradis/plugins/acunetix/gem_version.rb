9beb038e6f7c0de51c6fa7540d08cfa6c6d356d3
Daniel Martin <etd-github@nomejortu.com>
2017-03-13 10:29:13 +0100

    Prepare for dradis-plugins 3.6

--- lib/dradis/plugins/acunetix/gem_version.rb
+++ lib/dradis/plugins/acunetix/gem_version.rb
@@ -8,8 +8,8 @@ module Dradis
 
       module VERSION
         MAJOR = 3
-        MINOR = 2
-        TINY = 1
+        MINOR = 4
+        TINY = 0
         PRE = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
