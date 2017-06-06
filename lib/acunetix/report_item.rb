864ffa3b8d62ed481c91ec7022a139317113a827
Ryan Hays <hays.ryan@gmail.com>
2017-06-06 12:50:55 -0400

    Added a new sub to remove HTML from Acunetix evidence items.

--- lib/acunetix/report_item.rb
+++ lib/acunetix/report_item.rb
@@ -132,7 +132,8 @@ module Acunetix
 
       result.gsub!(/<li>(.*?)<\/li>/){"\n* #{$1.strip}"}
 
-      result.gsub!(/<span class="bb-dark"><strong>(.*?)<\/strong><\/span>/m){"#1.strip}"}
+      result.gsub!(/<span class="bb-dark"><strong>(.*?)<\/strong><\/span>/m){"#{$1.strip}\n"}
+      result.gsub!(/<span class="bb-navy">(.*?)<\/span>/m){"#{$1.strip}\n"}      
 
       result
     end
