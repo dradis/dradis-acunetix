3aed122fce2a6ab8fa3ec578f51555e57277d946
Sean Yeoh <sean.yeoh@hotmail.com>
2021-11-17 20:39:27 +0800

    Bump changelog and version

--- lib/dradis/plugins/acunetix/gem_version.rb
+++ lib/dradis/plugins/acunetix/gem_version.rb
@@ -8,8 +8,8 @@ module Dradis
 
       module VERSION
         MAJOR = 4
-        MINOR = 0
-        TINY = 0
+        MINOR = 1
+        TINY = 3
         PRE = nil
 
         STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
