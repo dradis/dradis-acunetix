90c534b36a22d75c78575b2e962259412a837a92
Brian Pearce <brian.o.pearce@gmail.com>
2022-03-17 14:52:19 +0100

    Register template mappings

--- lib/dradis/plugins/acunetix/engine.rb
+++ lib/dradis/plugins/acunetix/engine.rb
@@ -5,5 +5,9 @@ module Dradis::Plugins::Acunetix
     include ::Dradis::Plugins::Base
     description 'Processes Acunetix XML format'
     provides :upload
+
+    def self.template_names
+      { module_parent => { evidence: 'evidence', issue: 'report_item' } }
+    end
   end
 end
